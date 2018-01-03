9df07c05c509b21bad399ae1e204b20bbeb36d7d
Jim Stricker <jmstricker93@gmail.com>
2018-01-03 10:45:30 -0500

    turned App into container component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      filters: []
+    }
+  }
+
+  render(){
+    return(
+      <FruitBasket />
+    )
+  }
+
+}
 
 export default App;
