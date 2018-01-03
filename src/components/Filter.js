eff0d712d5bc7daf12a37e895045ff796281b09d
Jim Stricker <jmstricker93@gmail.com>
2018-01-03 11:40:52 -0500

    Filter now works as stateless functional component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 
-class Filter extends Component {
-
-
-  render() {
-    return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
-        <option value='all'>All</option>
-        {this.props.filters.map(filter =>
-          <option key={filter} value={filter}>{filter}</option>
-        )}
-      </select>
-    );
-  }
+const Filter = ({handleChange, filters}) => {
+  return(
+    <select onChange={handleChange} defaultValue='all'>
+      <option value='all'>All</option>
+      {filters.map(filter =>
+        <option key={filter} value={filter}>{filter}</option>
+      )}
+    </select>
+  )
 }
+
 
 Filter.defaultProps = {
   filters: [],
